ddb47b6c286d17e1e905c8123002556d94be5ce2
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-05-18 15:26:39 +0500

    Added basic menu bar and cursor

---
+++ index.js
@@ -0,0 +1,24 @@
+const { BrowserWindow, app } = require("electron");
+const { execSync } = require("child_process");
+
+try { execSync("taskkill /F /IM explorer.exe") } catch(e) { }
+
+app.on("ready", () => {
+  const win = new BrowserWindow({
+    fullscreen: true,
+    nodeIntegration: true,
+    webSecurity: false,
+    allowRunningInsecureContent: true,
+    contextIsolation: false,
+    kiosk: true,
+    minimizable: false,
+    resizable: false,
+    closable: false,
+    movable: false,
+    skipTaskbar: true,
+    frame: false,
+    autoHideMenuBar: true,
+    focusable: false,
+  });
+  win.loadFile("desktop/index.html");
+});
